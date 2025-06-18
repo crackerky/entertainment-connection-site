678299f47ae702644648847f3c5631995a942947
crackerky <crackert.ky@gmail.com>
2025-06-18 18:58:25 +0900

    Add HorizonHeroSection to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { HorizonHeroSection } from '@/components/ui/horizon-hero-section'
 import Mission from '@/components/sections/mission-new'
 import Vision from '@/components/sections/vision'
 import WhyUs from '@/components/sections/why-us'
@@ -6,6 +7,7 @@ import Achievements from '@/components/sections/achievements'
 export default function Home() {
   return (
     <>
+      <HorizonHeroSection />
       <Mission />
       <Vision />
       <WhyUs />
