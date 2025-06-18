1f17ba9479b7bdb58ab99c3ec38cf31a911a3b8f
crackerky <crackert.ky@gmail.com>
2025-06-18 18:46:19 +0900

    Update page to show only Mission, Vision, WhyUs, and Achievements sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,15 @@
-import Hero from '@/components/sections/hero'
 import Mission from '@/components/sections/mission-new'
 import Vision from '@/components/sections/vision'
 import WhyUs from '@/components/sections/why-us'
 import Achievements from '@/components/sections/achievements'
-import Services from '@/components/sections/services'
-import ClientShowcase from '@/components/sections/projects'
-import PricingPlans from '@/components/sections/features'
-import FAQ from '@/components/sections/mission'
-import Testimonials from '@/components/sections/testimonials'
-import Team from '@/components/sections/team'
-import CTA from '@/components/sections/cta'
-import Contact from '@/components/sections/contact'
 
 export default function Home() {
   return (
     <>
-      <Hero />
       <Mission />
       <Vision />
       <WhyUs />
       <Achievements />
-      <Services />
-      <ClientShowcase />
-      <PricingPlans />
-      <FAQ />
-      <Testimonials />
-      <Team />
-      <CTA />
-      <Contact />
     </>
   )
 }
